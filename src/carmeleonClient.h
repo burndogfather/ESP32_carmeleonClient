b84e552e44cbc515d67ea98cbcb8ede386f3f29b
coco <admin@burndogfather.com>
2025-04-10 21:13:57 +0900

    2025-04-10 21:13:57 업데이트!!!

--- src/carmeleonClient.h
+++ src/carmeleonClient.h
@@ -2,10 +2,12 @@
 #define CARMELEON_CLIENT_H
 
 #include <Ethernet/EthernetESP32.h>
+#include <https/HttpSecure.h>
 
 class carmeleonClient {
 public:
   EthernetClass Ethernet;  // ✅ 내부 멤버로 선언
+  HttpSecure http;
 
   carmeleonClient();
 
