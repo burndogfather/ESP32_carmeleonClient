9c73b9316baa5f3def231bc39fc80a51440aff7e
coco <admin@burndogfather.com>
2025-04-10 20:22:58 +0900

    2025-04-10 20:22:58 업데이트!!!

--- src/carmeleonClient.h
+++ src/carmeleonClient.h
@@ -3,12 +3,13 @@
 
 #include <Ethernet/EthernetESP32.h>
 
-class carmeleonClient : public EthernetClass {
+class carmeleonClient {
 public:
+  EthernetClass Ethernet;  // ✅ 내부 멤버로 선언
+
   carmeleonClient();
 
   bool connectToServer(const char* host, uint16_t port);
-  bool sendPing();
   void loop();
 };
 
