b1c5600512d7ee6875db7eb3b339433371f5ded0
coco <admin@burndogfather.com>
2025-04-10 21:12:57 +0900

    2025-04-10 21:12:57 업데이트!!!

--- src/Https/HttpSecure.cpp
+++ src/Https/HttpSecure.cpp
@@ -1,19 +1,54 @@
-#ifndef CARMELEON_CLIENT_H
-#define CARMELEON_CLIENT_H
+#include "HttpSecure.h"
 
-#include <Arduino.h>
-#include <EthernetESP32.h>
+HttpSecure::HttpSecure() {
+  mbedtls_ssl_init(&_ssl);
+  mbedtls_ssl_config_init(&_conf);
+  mbedtls_ctr_drbg_init(&_ctr_drbg);
+}
 
-class carmeleonClient {
-public:
-  EthernetClass Ethernet;
-  EthernetClient netClient;
-  carmeleonHttpSecure http;
+bool HttpSecure::begin(const char* fullUrl) {
+  // URL 파싱 로직 구현
+  // _host, _path, _port 설정
+  // mbedTLS 설정 및 소켓 연결
+}
 
-  carmeleonClient();
-  void init(EthDriver& driver);
-  bool begin(uint8_t* mac);
-  void loop();
-};
+void HttpSecure::header(const String& name, const String& value) {
+  _headers[name] = value;
+}
 
-#endif
+int HttpSecure::get() {
+  sendRequest("GET");
+  readResponse();
+  return _statusCode;
+}
+
+int HttpSecure::post(const String& body, const String& contentType) {
+  sendRequest("POST", body, contentType);
+  readResponse();
+  return _statusCode;
+}
+
+String HttpSecure::responseBody() {
+  return _response;
+}
+
+int HttpSecure::statusCode() {
+  return _statusCode;
+}
+
+void HttpSecure::end() {
+  mbedtls_ssl_close_notify(&_ssl);
+  mbedtls_ssl_free(&_ssl);
+  mbedtls_ssl_config_free(&_conf);
+  mbedtls_ctr_drbg_free(&_ctr_drbg);
+  close(_socket);
+  _connected = false;
+}
+
+void HttpSecure::sendRequest(const String& method, const String& body, const String& contentType) {
+  // HTTP 요청 전송 로직 구현
+}
+
+void HttpSecure::readResponse() {
+  // HTTP 응답 읽기 로직 구현
+}
