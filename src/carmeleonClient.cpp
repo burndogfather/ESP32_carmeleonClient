dbebbf5385f17e0b699dd985efb23f8cd2dbbd80
coco <admin@burndogfather.com>
2025-04-10 20:21:58 +0900

    2025-04-10 20:21:58 업데이트!!!

--- src/carmeleonClient.cpp
+++ src/carmeleonClient.cpp
@@ -1,20 +1,20 @@
 #include "carmeleonClient.h"
 
 carmeleonClient::carmeleonClient() {
-  // 생성자에서 기본 설정 가능
+  // 생성자에서 특별히 할 일 없음
 }
 
 bool carmeleonClient::connectToServer(const char* host, uint16_t port) {
   IPAddress ip;
-  if (!hostByName(host, ip)) {
-	Serial.println("DNS 실패");
-	return false;
+  if (!Ethernet.hostByName(host, ip)) {
+    Serial.println("DNS 실패");
+    return false;
   }
 
   EthernetClient client;
   if (!client.connect(ip, port)) {
-	Serial.println("서버 연결 실패");
-	return false;
+    Serial.println("서버 연결 실패");
+    return false;
   }
 
   Serial.println("서버 연결 성공");
@@ -22,17 +22,6 @@ bool carmeleonClient::connectToServer(const char* host, uint16_t port) {
   return true;
 }
 
-bool carmeleonClient::sendPing() {
-  // 네트워크 상태 확인
-  if (linkStatus() != LinkON) {
-	Serial.println("네트워크 링크 꺼짐");
-	return false;
-  }
-
-  // ping처럼 간단한 요청을 보낼 수 있음
-  return true;
-}
-
 void carmeleonClient::loop() {
-  maintain(); // DHCP 리스 연장
+  Ethernet.maintain();
 }
